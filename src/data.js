ead0eeff05716bda9e71e66bd45d3ed9a12e6016
s-hens <108551327+s-hens@users.noreply.github.com>
2023-06-13 18:30:57 +0100

    Add error handling to API call

--- src/data.js
+++ src/data.js
@@ -13,11 +13,24 @@ function getLocale() {
 
 // API call
 async function callAPI(locale) {
-    const APIresponse = await fetch(`https://api.weatherapi.com/v1/current.json?key=8149f881dcf243d0b39105948233005&q=${locale}&aqi=no`, {mode: "cors"});
-    const data = await APIresponse.json();
-    lastData = data;
-    printData(data);
-    displaySky(data);
+    try {
+        const APIresponse = await fetch(`https://api.weatherapi.com/v1/current.json?key=8149f881dcf243d0b39105948233005&q=${locale}&aqi=no`, {mode: "cors"});
+        const data = await APIresponse.json();
+        lastData = data;
+        printData(data);
+        displaySky(data);
+    } catch {
+        const errorAPIresponse = await fetch(`https://api.weatherapi.com/v1/current.json?key=8149f881dcf243d0b39105948233005&q=${locale}&aqi=no`, {mode: "cors"});
+        const errorData = await errorAPIresponse.json();
+        //console.log(errorData);
+        if (errorData.error.code == 1003) {
+            alert(`Error: Please enter a location to search.`);
+        } else if (errorData.error.code == 1006) {
+            alert(`Error: No matches found for ${locale}. Please try a different search term.`);
+        } else {
+            alert(`Error: ${errorData.error.message}`);
+        }
+    }
 }
 
 export { getLocale, callAPI, lastData };
